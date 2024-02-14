aedba68cc23d92d7e63187d673cc92630fe57b81
Sergey <Dolorem.scire@gmail.com>
2024-02-14 15:25:21 +0300

    Реализовал маршрутизацию на проекте

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 // const StyledApp = styled.main`
 //     background-color: red;
@@ -16,9 +25,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
     return (
         <>
+            <GlobalStyles></GlobalStyles>
             <BrowserRouter>
                 <Routes>
-                    <Route path="dashboard" element={1} />
+                    <Route
+                        index
+                        element={<Navigate replace to={"/dashboard"} />}
+                    />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="bookings" element={<Bookings />} />
+                    <Route path="cabins" element={<Cabins />} />
+                    <Route path="users" element={<Users />} />
+                    <Route path="settings" element={<Settings />} />
+                    <Route path="account" element={<Account />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
         </>
